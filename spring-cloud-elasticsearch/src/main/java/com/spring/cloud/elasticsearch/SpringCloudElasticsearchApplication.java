373b7f7942a9b08f31dd921094072d5b9b499163
aihaozai <1020146023@qq.com>
2021-04-20 21:46:08 +0800

    Elasticsearch

---
+++ spring-cloud-elasticsearch/src/main/java/com/spring/cloud/elasticsearch/SpringCloudElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudElasticsearchApplication.class, args);
+    }
+
+}
