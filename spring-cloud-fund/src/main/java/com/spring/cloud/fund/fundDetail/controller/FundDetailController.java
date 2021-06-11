a43bcb4800565b8c538434535b1178093ef09305
zhenjh <aihaozai@gmail.com>
2021-06-11 15:41:29 +0800

    基金详细表

--- spring-cloud-fund/src/main/java/com/spring/cloud/fund/fundDetail/controller/FundDetailController.java
+++ spring-cloud-fund/src/main/java/com/spring/cloud/fund/fundDetail/controller/FundDetailController.java
@@ -2,7 +2,6 @@ package com.spring.cloud.fund.fundDetail.controller;
 
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.spring.cloud.fund.fund.entity.Fund;
 import com.spring.cloud.fund.fund.service.IFundService;
 import com.spring.cloud.fund.fundDetail.entity.FundDetail;
 import com.spring.cloud.fund.fundDetail.service.IFundDetailService;
@@ -10,13 +9,9 @@ import com.spring.cloud.fund.handler.SearchFundJobHandler;
 import io.swagger.annotations.Api;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import spring.cloud.base.core.result.Result;
 import spring.cloud.base.datasource.request.QueryPage;
-import spring.cloud.base.fund.util.FundDataUtil;
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -30,16 +25,13 @@ import java.util.List;
 @AllArgsConstructor
 public class FundDetailController {
 
-    @Value("${search.fundCode}")
-    private ArrayList<String> fundCodeList;
-
-    private final IFundService iFundService;
+    private final IFundDetailService iFundDetailService;
 
     private final SearchFundJobHandler searchFundJobHandler;
 
     @PostMapping("/page")
     public Result<Page<FundDetail>> page(@RequestBody QueryPage queryPage) {
-        Page<FundDetail> page = iFundService.page(new Page<>(queryPage.getCurrent(), queryPage.getCurrent()),queryPage.getQueryWrapper());
+        Page<FundDetail> page = iFundDetailService.page(new Page<>(queryPage.getCurrent(), queryPage.getCurrent()),queryPage.getQueryWrapper());
         return Result.ok(page);
     }
 
