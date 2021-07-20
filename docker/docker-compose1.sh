8ae7d564d9f9cb2b552f502fc1256f1faae4ceed
zhenjh <aihaozai@gmail.com>
2021-07-20 13:52:03 +0800

    fix: docker  脚本

---
+++ docker/docker-compose1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /data/spring-cloud-haozai/
+project_path=$(cd `dirname $0`; pwd)
+git pull
+mvn clean package -Dmaven.test.skip=true
+cd docker
+docker-compose -f docker-compose1.yml  up --build -d
+docker images
+docker rmi $(docker images | grep none | awk '{print $3}')
