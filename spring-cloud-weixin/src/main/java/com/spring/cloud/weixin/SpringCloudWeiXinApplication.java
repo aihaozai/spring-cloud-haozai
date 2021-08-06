1deb99bbcdb801161ff58c5466ef9dea7275f427
aihaozai <1020146023@qq.com>
2021-08-06 21:20:58 +0800

    feat: 微信服务  统一版本

---
+++ spring-cloud-weixin/src/main/java/com/spring/cloud/weixin/SpringCloudWeiXinApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudWeiXinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudWeiXinApplication.class, args);
+    }
+
+}
