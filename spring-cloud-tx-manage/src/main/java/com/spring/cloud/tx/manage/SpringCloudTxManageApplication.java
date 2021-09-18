8763812bc539d4f72289c31717f273359db01a98
zhenjh <aihaozai@gmail.com>
2021-09-18 17:53:18 +0800

    feat: tx-manage

---
+++ spring-cloud-tx-manage/src/main/java/com/spring/cloud/tx/manage/SpringCloudTxManageApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.tx.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTxManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTxManageApplication.class, args);
+    }
+
+}
