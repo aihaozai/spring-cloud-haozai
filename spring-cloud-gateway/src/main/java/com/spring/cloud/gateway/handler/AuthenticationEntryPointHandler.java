f4bc833a16e785e76dbff293e9395683a031b6ea
zhenjh <aihaozai@gmail.com>
2021-12-10 17:54:03 +0800

    fix:认证

--- spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/handler/AuthenticationEntryPointHandler.java
+++ spring-cloud-gateway/src/main/java/com/spring/cloud/gateway/handler/AuthenticationEntryPointHandler.java
@@ -1,12 +1,16 @@
 package com.spring.cloud.gateway.handler;
 
+
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.server.ServerAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
@@ -17,12 +21,16 @@ import reactor.core.publisher.Mono;
 @Slf4j
 @Component
 public class AuthenticationEntryPointHandler implements ServerAuthenticationEntryPoint {
+
+    private static final JSONObject RESPONSE = new JSONObject();
+
     @Override
     public Mono<Void> commence(ServerWebExchange serverWebExchange, AuthenticationException e) {
-        log.info("认证入口---请求链接:{},原因:{}", serverWebExchange.getRequest().getPath(), e.getMessage());
-        String url = "http://localhost:8080";
-        serverWebExchange.getResponse().setStatusCode(HttpStatus.SEE_OTHER);
-        serverWebExchange.getResponse().getHeaders().set(HttpHeaders.LOCATION, url);
-        return serverWebExchange.getResponse().setComplete();
+        log.error("认证失败---请求链接:{},原因:{}", serverWebExchange.getRequest().getPath(), e.getMessage());
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+
+        RESPONSE.put("msg", e.getMessage());
+        DataBufferFactory bufferFactory = serverWebExchange.getResponse().bufferFactory();
+        return serverWebExchange.getResponse().writeWith(Flux.just(bufferFactory.wrap(RESPONSE.toString().getBytes())));
     }
 }
